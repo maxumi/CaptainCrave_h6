9e58a9e52ce19f07bb3de15661dbdc8808ebe6d4
maxumi <maxjac64@gmail.com>
2026-08-07 08:28:09 +0200

    forget to update app config

--- Client/src/app/app.config.ts
+++ Client/src/app/app.config.ts
@@ -1,11 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideTransloco } from '@jsverse/transloco';
+import { TranslocoHttpLoader } from '../transloco-loader';
+import { providePreloadUserLang } from '../preload-user-lang';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideTransloco({
+        config: { 
+          availableLangs: ['en', 'da'],
+          defaultLang: 'en',
+          // Remove this option if your application doesn't support changing language in runtime.
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      }),
+      providePreloadUserLang()
   ]
 };
