0db7a86f0f74dd70efd09f231eb46ed3de4f6531
maxumi <maxjac64@gmail.com>
2026-08-14 09:26:21 +0200

    Angular: Added Proxy for aspire

---
+++ Client/src/proxy.conf.js
@@ -0,0 +1,15 @@
+// src/proxy.conf.js
+
+const env = process.env;
+
+const target =
+    env["services__api__https__0"] ??
+    env["services__api__http__0"];
+
+module.exports = {
+    "/api/**": {
+        target,
+        secure: false,
+        changeOrigin: true
+    }
+};
